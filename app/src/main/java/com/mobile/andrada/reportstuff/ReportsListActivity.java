66d24be6c25e2804a6fe6b8a97c62a9e3bbcae6b
Andrada <gaie2122@scs.ubbcluj.com>
2019-04-02 08:55:39 +0300

    Config files

---
+++ app/src/main/java/com/mobile/andrada/reportstuff/ReportsListActivity.java
@@ -0,0 +1,13 @@
+package com.mobile.andrada.reportstuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reports_list);
+    }
+}
