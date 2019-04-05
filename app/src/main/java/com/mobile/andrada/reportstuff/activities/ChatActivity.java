8858a842ee6a8c01557ff44d13beee98101ffa7e
Andrada <gaie2122@scs.ubbcluj.com>
2019-04-05 14:55:11 +0300

    Set up navigation between activities

--- app/src/main/java/com/mobile/andrada/reportstuff/activities/ChatActivity.java
+++ app/src/main/java/com/mobile/andrada/reportstuff/activities/ChatActivity.java
@@ -1,15 +1,37 @@
 package com.mobile.andrada.reportstuff.activities;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.mobile.andrada.reportstuff.R;
 
 public class ChatActivity extends AppCompatActivity {
+    public final static String EXTRA_ID = "com.mobile.andrada.reportstuff.activities.EXTRA_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Chat");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
