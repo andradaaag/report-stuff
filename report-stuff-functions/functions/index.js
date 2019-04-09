02a1b6c92d6e00d6d3c490b9889d99bff5b81a92
Andrada <gaie2122@scs.ubbcluj.com>
2019-04-09 11:23:19 +0300

    Fixed function - works once but crashes client app

--- report-stuff-functions/functions/index.js
+++ report-stuff-functions/functions/index.js
@@ -7,14 +7,14 @@ admin.initializeApp();
 
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+exports.makeUppercase = functions.database.ref('/messages/{pushId}')
     .onCreate((snapshot, context) => {
-    // Grab the current value of what was written to the Realtime Database.
-    const original = snapshot.val();
-console.log('Uppercasing', context.params.pushId, original);
-const uppercase = original.toUpperCase();
-// You must return a Promise when performing asynchronous tasks inside a Functions such as
-// writing to the Firebase Realtime Database.
-// Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-return snapshot.ref.parent.child('uppercase').set(uppercase);
-});
+        // Grab the current value of what was written to the Realtime Database.
+        const original = snapshot.val();
+        console.log('Uppercasing', context.params.pushId, original);
+        const uppercase = original.text.toUpperCase();
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+        // writing to the Firebase Realtime Database.
+        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+        return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
