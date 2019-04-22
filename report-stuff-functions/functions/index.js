e59523f7f38f07f3ed1c765a17a3e290dfb564cb
Andrada <gaeandrada@gmail.com>
2019-04-22 14:00:34 +0300

    Created functions for granting policeman role

--- report-stuff-functions/functions/index.js
+++ report-stuff-functions/functions/index.js
@@ -5,16 +5,27 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}')
-    .onCreate((snapshot, context) => {
-        // Grab the current value of what was written to the Realtime Database.
-        const original = snapshot.val();
-        console.log('Uppercasing', context.params.pushId, original);
-        const uppercase = original.text.toUpperCase();
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to the Firebase Realtime Database.
-        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-        return snapshot.ref.parent.child('uppercase').set(uppercase);
+
+exports.makePoliceman = functions.https.onCall((data, context) => {
+    if (context.auth.token.policeman !== true) {
+        return {
+            error: "Request not authorized. User must be a policeman to fulfill request."
+        };
+    };
+    const email = data.email;
+    return grantPolicemanRole(email).then(() => {
+        return {
+            result: `Request fulfilled! ${email} is now a policeman.`
+        };
     });
+});
+
+async function grantPolicemanRole(email) {
+    const user = await admin.auth().getUserByEmail(email);
+    if (user.customClaims && user.customClaims.policeman === true) {
+        return;
+    }
+    return admin.auth().setCustomUserClaims(user.uid, {
+        policeman: true
+    });
+}
