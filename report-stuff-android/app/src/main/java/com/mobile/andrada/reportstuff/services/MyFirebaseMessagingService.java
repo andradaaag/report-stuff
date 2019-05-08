e502f78f26c6bb0f165617bcf5194a0d1f9e9a7e
Andrada <gaeandrada@gmail.com>
2019-05-08 16:48:49 +0300

    Fixed showing of notification, it needed a channel configuration

--- report-stuff-android/app/src/main/java/com/mobile/andrada/reportstuff/services/MyFirebaseMessagingService.java
+++ report-stuff-android/app/src/main/java/com/mobile/andrada/reportstuff/services/MyFirebaseMessagingService.java
@@ -1,13 +1,16 @@
 package com.mobile.andrada.reportstuff.services;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.mobile.andrada.reportstuff.R;
 
 import java.util.Map;
 
@@ -31,17 +34,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private void showNotification(Map<String, String> payload) {
         String text = payload.get("citizenName") + payload.get("location") + payload.get("time");
 
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = notificationManager.getNotificationChannel("channel_id");
+            if (mChannel == null) {
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                mChannel = new NotificationChannel("channel_id", "NotificationChannel", importance);
+                mChannel.enableVibration(true);
+                notificationManager.createNotificationChannel(mChannel);
+            }
+        }
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
                 .setContentTitle("New Report Nearby")
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setStyle(new NotificationCompat.BigTextStyle())
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
-//                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher);
 //                .setAutoCancel(true);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, notificationBuilder.build());
     }
