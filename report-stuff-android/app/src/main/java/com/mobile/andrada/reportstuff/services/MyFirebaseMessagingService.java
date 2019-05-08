ded858a44cf2eb529424fd98187bf7c73cf7016a
Andrada <gaeandrada@gmail.com>
2019-05-08 13:05:37 +0300

    Show notification on user's device

--- report-stuff-android/app/src/main/java/com/mobile/andrada/reportstuff/services/MyFirebaseMessagingService.java
+++ report-stuff-android/app/src/main/java/com/mobile/andrada/reportstuff/services/MyFirebaseMessagingService.java
@@ -1,10 +1,15 @@
 package com.mobile.andrada.reportstuff.services;
 
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -16,12 +21,35 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
         Log.d(TAG, "FCM Notification Message: " + remoteMessage.getNotification());
         Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+
+        if (remoteMessage.getData().size() > 0) {
+            Map<String, String> payload = remoteMessage.getData();
+            showNotification(payload);
+        }
+    }
+
+    private void showNotification(Map<String, String> payload) {
+        String text = payload.get("location"); //TODO: add more info on notification
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle("New Report Nearby")
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setAutoCancel(true);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
     }
 
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
         Log.d("NEW_TOKEN", s);
-        //TODO: call cloud function for updating token in officials collection
+        //TODO: update token in officials collection
     }
 }
